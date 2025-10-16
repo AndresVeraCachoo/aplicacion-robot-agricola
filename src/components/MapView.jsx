35d2dc3165bb848006e318581629e0016f910f0a
Andrés Vera <andresveracacho@gmail.com>
2025-10-16 21:49:25 +0200

    feat: Implementa la UI de la página principal del Dashboard

---
+++ src/components/MapView.jsx
@@ -0,0 +1,19 @@
+// src/components/MapView.jsx
+import React from 'react';
+import './MapView.css';
+
+function MapView() {
+  return (
+    <main className="map-view-container">
+      <div className="field-representation">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+    </main>
+  );
+}
+
+export default MapView;
